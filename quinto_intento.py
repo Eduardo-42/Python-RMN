ff5cac73f126db0e1686b3c7b24b7978c42045eb
Eduardo-42 <60886478+Eduardo-42@users.noreply.github.com>
2020-11-24 17:33:04 -0600

    Codigo corregido y comentado
    Liat hice unas correcciones al codigo solo le añadi lineas para que puedas visualizar la información borrare el otro

--- quinto_intento.py
+++ quinto_intento.py
@@ -12,6 +12,31 @@ print(td.J)
 
 grafica = mplplot(td.peaklist())
 
-grafica.get_xdata()
-grafica.get_ydata()
-grafica.get_xydata()
+print(type(grafica)) #imprime el tipo de grafica 
+
+print(grafica[1]) #imprime el segundo valor de la tupla grafica que son las intensidades en forma de lista
+
+print(f'la longitud de los valores de X en grafica 1 son: {len(grafica[1])}')
+print(f'la longitud de los valores de Y en grafica 1 son: {len(grafica[0])}')
+
+#DEBIDO A QUE SON DIMENSIONES IGUALES EL ORDENAMIENTO DE TUPLAS ES 1 a 1 por lo que
+
+axe_x = grafica[0]
+axe_y = grafica[1]
+
+x=grafica[1] #Muestra de que el array Y muestra las intensidades sin se rnormalizados
+
+plt.plot(x) #La grafica muestra el mismo valor que [grafica]
+plt.show()
+
+print(td.peaklist()) #Te imprime la lista de los valores correspondientes a la RMN en forma de lista que guarda tuplas de pares
+
+print(type(td.peaklist()))
+
+axes = plt.gca()
+
+line = axes.lines[0]
+
+line.get_xdata()
+#grafica.get_ydata()
+grafica.get_xydata() 
